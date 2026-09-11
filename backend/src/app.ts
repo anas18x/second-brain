17442dc394798ba52377ff1be51dd70821c354bb
anas18x <anasshakil00@gmail.com>
2026-09-11 17:29:28 +0530

    chore: add database health check

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,21 +1,29 @@
 import express from "express"
 import cookieParser from "cookie-parser"
-import {rateLimit} from "express-rate-limit"
+import { rateLimit } from "express-rate-limit"
 import cors from "cors"
-import {ENV } from "./config/ENV.config.js"
+import mongoose from "mongoose"
+
+import { ENV } from "./config/ENV.config.js"
 import v1Routes from "./routes/v1/index.js"
 import errorMiddleware from "./middleware/error.Middleware.js"
-
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   limit: 100,
   skip: (req) => req.method === "OPTIONS",
-  message: "Too many requests from this IP, please try again later.",
+  message: "Too many requests from IP, please try again later.",
 })
 
 const app = express()
+
 app.set("trust proxy", 1)
+
+app.get("/api/v1/health/db", (_req, res) => {
+  res.json({
+    mongoReadyState: mongoose.connection.readyState,
+  })
+})
 
 app.use(express.json())
 app.use(cookieParser())
